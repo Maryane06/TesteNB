3be88ca86a0d0d428efd13d41867ef959e7c064a
Maryane06 <maryaneqs@gmail.com>
2024-08-06 13:40:04 -0300

    Add files via upload

---
+++ ApplicationMessagedialog/src/applicationmessagedialog/ApplicationMessagedialog.java
@@ -0,0 +1,18 @@
+
+package applicationmessagedialog;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author maryane.squeiroz1
+ */
+public class ApplicationMessagedialog {
+
+    public static void main(String[] args) {
+        ImageIcon java = new ImageIcon ("C:\\Users\\maryane.squeiroz1\\Downloads\\java.png");
+JOptionPane.showMessageDialog(null, "bem-vindos!", "SENAC",0,java );
+    }
+    
+}
