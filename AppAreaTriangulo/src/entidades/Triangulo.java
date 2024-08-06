673385cb5aec7ca8ca38f3fc3832bef08924bfbc
Maryane06 <maryaneqs@gmail.com>
2024-08-06 13:58:04 -0300

    Add files via upload

---
+++ AppAreaTriangulo/src/entidades/Triangulo.java
@@ -0,0 +1,11 @@
+
+package entidades;
+
+public class Triangulo {
+   public double base;
+   public double altura;
+   
+   public double areaTriangulo(){
+       return(base*altura)/2.00;
+   }
+}
